226e88bdb74de570ce729f72ac1b385fd3f6578e
林鋒諺 <s113029021@smail.nchu.edu.tw>
2025-02-08 14:52:23 +0800

    Add files via upload
    原本想要試著找出有最大值的規律，但後來找不到想說直接暴力解就好。暴力解的想法我原本想要先把所有的結果存到vector再sort，但chatgpt說可以先宣告一個maxnum再每次產出f(i)的時候比較，這樣比較快。然後要小心輸入的兩個數字的大小問題。

---
+++ UVA100 - The 3n + 1 problem.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f(int a){
+	int n = 0;
+	while(true){
+		n += 1;
+		if(a == 1) return n;
+		else if(a % 2 == 0) a = a / 2;
+		else a = 3 * a + 1;
+	}
+}
+
+int main(){
+	//ios::sync_with_stdio(0); cin.tie(0);
+	int a, b;
+	while(cin >> a >> b){
+		int begin = min(a, b), end = max(a, b);
+		int maxnum = 0;
+		for(int i = a;i <= b;i++) maxnum = max(maxnum, f(i));
+		cout << a << ' ' << b << ' ' << maxnum << '\n';
+	}
+}

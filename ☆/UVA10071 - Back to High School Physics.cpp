07fe1fd14f2659b9b45dde0d51aef27c079cf9c0
林鋒諺 <s113029021@smail.nchu.edu.tw>
2025-02-26 19:13:19 +0800

    Add files via upload

---
+++ ☆/UVA10071 - Back to High School Physics.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int v, t;
+    while(cin >> v >> t){
+        int d = v * t * 2;
+        cout << d << '\n';
+    }
+}
